b091e0e5d7dab6385ecf994a1e9170caf05059e4
Luis <le.cervantes.96@gmail.com>
2023-01-11 16:10:34 -0600

    retoSemanal8

---
+++ retoSemanal8.py
@@ -0,0 +1,37 @@
+english_colors = {
+    "rojo": "red",
+    "naranja": "orange",
+    "amarillo": "yellow",
+    "verde": "green",
+    "azul": "blue",
+    "violet": "purple"
+}
+german_colors = {
+    "rojo": "rot",
+    "naranja": "orange",
+    "amarillo": "gelb",
+    "verde": "grün",
+    "azul": "blau",
+    "violet": "violett"
+}
+response = ""
+
+print("Bienvenid@, esta aplicación te permite traducir al inglés o al alemán los colores que ingreses.")
+option = input("¿Quieres el color en ingles (I) o en alemán (A)? ")
+
+if option != "I" and option != "i" and option != "A" and option != "a":
+    print("Se ha ingresado una opcion no válida")
+    exit()
+
+color = input("Ingresa uno de los colores del arcoiris en español: ")
+color.lower()
+newColor = color.split()
+
+for color in newColor:
+    if (option == "i" or option == "I") and color in english_colors:
+        response = response +"< "+ english_colors[color] + " >"
+    elif (option == "a" or option == "A") and color in german_colors:
+        response = response +"< "+ german_colors[color] + " >"
+    else:
+        response = response + color + " "
+print(response)
